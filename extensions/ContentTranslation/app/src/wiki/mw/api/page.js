51bc24f486680e1c2a94ce339e5c1e73cf9098cd
ibrahem <ibrahem.al-radaei@outlook.com>
2024-08-12 00:11:42 +0300

    Support Translation from mdwiki.org

--- extensions/ContentTranslation/app/src/wiki/mw/api/page.js
+++ extensions/ContentTranslation/app/src/wiki/mw/api/page.js
@@ -148,12 +148,61 @@ const fetchPageContent = (
  * @param {string|null} revision
  * @return {Promise<String>}
  */
+const fetchSegmentedContent_mdwiki = (
+    sourceLanguage,
+    targetLanguage,
+    sourceTitle,
+    revision = null
+) => {
+    const title = sourceTitle.replace(/ /g, "_")
+    const sourceWikiCode = sourceLanguage;
+    const targetWikiCode = siteMapper.getWikiDomainCode(targetLanguage);
+
+    const cxServerParams = {
+        sourcelanguage: sourceWikiCode,
+        targetlanguage: targetWikiCode,
+    };
+
+    var cxServerApiURL = "https://medwiki.toolforge.org/get_html.php";
+
+    // If revision is requested, load that revision of page.
+    if (revision) {
+        cxServerParams.revision = revision;
+    } else {
+        cxServerParams.title = title;
+    }
+    const options = {
+        method: 'GET',
+        dataType: 'json'
+    }
+
+    cxServerApiURL = cxServerApiURL + "?" + $.param(cxServerParams)
+
+    const result = fetch(cxServerApiURL, options)
+        .then((response) => response.json())
+        .then((response) => response.segmentedContent);
+
+    return result;
+};
+
+/**
+ * Fetches segmented content of a page for given source language,
+ * target language and source title.
+ * @param {string} sourceLanguage
+ * @param {string} targetLanguage
+ * @param {string} sourceTitle
+ * @param {string|null} revision
+ * @return {Promise<String>}
+ */
 const fetchSegmentedContent = (
     sourceLanguage,
     targetLanguage,
     sourceTitle,
     revision = null
 ) => {
+    if (sourceLanguage === "mdwiki") {
+        return fetchSegmentedContent_mdwiki(sourceLanguage, targetLanguage, sourceTitle, revision);
+    }
     const sourceWikiCode = siteMapper.getWikiDomainCode(sourceLanguage);
     const targetWikiCode = siteMapper.getWikiDomainCode(targetLanguage);
     const cxServerParams = {
