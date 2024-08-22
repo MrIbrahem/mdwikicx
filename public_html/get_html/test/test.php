542a3d548f1c82a286acc3aa84a32f74176f8e60
ibrahem <ibrahem.al-radaei@outlook.com>
2024-08-22 06:02:22 +0300

    Improve text processing and prepare for translation
    Improve text processing and prepare for translation

--- public_html/get_html/test/test.php
+++ public_html/get_html/test/test.php
@@ -3,14 +3,15 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require __DIR__ . "/m.php";
+require __DIR__ . "/../m.php";
 
 // get old.html
 $old = file_get_contents(__DIR__ . "/old.html");
 $new = $old;
 file_put_contents(__DIR__ . "/new.html", $new);
 
-$new = do_changes($old, false);
+// $new = do_changes($old, false);
+$new = dom_it($old);
 
 // $new = remove_unlinkedwikibase($new);
 // <span about="#mwt1" typeof="mw:Transclusion" data-mw='{"parts":[{"template":{"target":{"wt":"#unlinkedwikibase:id=Q117768481","function":"unlinkedwikibase"},"params":{},"i":0}}]}' id="mwAg"></span>
